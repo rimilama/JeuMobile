411546e9ea5007586248cf45ba6047a1374940ef
Rimilya <a.daeffler@ludus-academie.com>
2021-02-01 13:05:34 +0100

    Premier ajout
    - Assets (personnage/background/tire)
    - Déplacement du personnage (au clavier)
    - Tir (au clavier)

---
+++ MobileGame/Assets/Script/ControllingCameraAspectScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code provided by 
+public class ControllingCameraAspectScript : MonoBehaviour
+{
+    void Start()
+    {
+        //Target ratio
+        float targetaspect = 16f / 9f;
+
+        //Get current width and height
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+
+        //Calculates the height to apply
+        float scaleheight = windowaspect / targetaspect;
+
+        Camera camera = GetComponent<Camera>();
+
+        //Changes camera's rect to match given ratio
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+    }
+
+}
---
+++ MobileGame/Assets/Script/Mouvement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvement : MonoBehaviour
+{
+    public GameObject bullet;
+    private Rigidbody2D rb;
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float shootRate;
+    private float nextShoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (nextShoot > 0)
+            {
+                nextShoot -= Time.deltaTime;
+            }
+            if(nextShoot <= 0)
+            {
+                Shoot();
+            }
+        }
+        Deplacement();
+        Rotation();
+    }
+
+    void Deplacement()
+    {
+        Vector2 InputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.AddForce(InputDir * moveSpeed);
+    }
+
+    void Rotation()
+    {
+        float Angle = Mathf.Atan2(Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal"))*Mathf.Rad2Deg-90;
+        Quaternion rotation = Quaternion.AngleAxis(Angle, Vector3.forward);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+
+    void Shoot()
+    {
+        nextShoot = shootRate;
+        GameObject bulletClone = Instantiate(bullet, new Vector2(bullet.transform.position.x, bullet.transform.position.y), transform.rotation);
+        bulletClone.SetActive(true);
+        bulletClone.GetComponent<Bullet>().killClone();
+    }
+}
